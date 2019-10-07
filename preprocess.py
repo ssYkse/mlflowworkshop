4e3250ca9e57162f8bc5aa2376a8c9e7e3892d3b
Yannick Saive <yannick.saive@soprasteria.com>
2019-10-07 12:30:21 -0400

    move things into __main__

--- preprocess.py
+++ preprocess.py
@@ -1,42 +1,41 @@
 import numpy as np
 from os.path import join as opj
-import argparse
 import os
 import tempfile
 import mlflow
 
-parser = argparse.ArgumentParser(description='Preprocess all data.')
-parser.add_argument('rawdir', type=str, 
-                    help='Directory where the raw data is found.')
-parser.add_argument('mean', type=float, 
-                    help='Mean for normalization.')
-parser.add_argument('std', type=float, 
-                    help='Std for normalization.')
 
-args = parser.parse_args()
 
-tempdir = tempfile.mkdtemp()
+def preprocess(x_train, mean, std):
+    #Normalize data
+    x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1).astype('float32')
+    x_train = x_train - mean
+    x_train = x_train / std
 
-#TODO: make preprocess function
-def preprocess():
-    x_train = x_train - args.mean
-    x_train = x_train / args.std
+    return x_train
+
 
 if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Preprocess all data.')
+    parser.add_argument('rawdir', type=str, 
+                        help='Directory where the raw data is found.')
+    parser.add_argument('mean', type=float, 
+                        help='Mean for normalization.')
+    parser.add_argument('std', type=float, 
+                        help='Std for normalization.')
+    args = parser.parse_args()
+    tempdir = tempfile.mkdtemp()
+
     #Open data
     x_train = np.load(opj(args.rawdir, 'x_train.npy'))
     y_train = np.load(opj(args.rawdir, 'y_train.npy'))
     x_test = np.load(opj(args.rawdir, 'x_test.npy'))
     y_test = np.load(opj(args.rawdir, 'y_test.npy'))
 
-    #Normalize data
-    x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1).astype('float32')
-    x_train = x_train - args.mean
-    x_train = x_train / args.std
-
-    x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1).astype('float32')
-    x_test = x_test - args.mean
-    x_test = x_test / args.std
+    #Do the preprocessing
+    x_train= preprocess(x_train, args.mean, args.std)
+    x_test = preprocess(x_test , args.mean, args.std)
 
     #Save data
     np.save(opj(tempdir, 'x_train'), x_train)
